4323e7fc7e72ea7a2ce73558112944033d3514a8
calexoz <156954450+calexoz@users.noreply.github.com>
2024-01-22 19:43:52 -0600

    Se genera script para obtener numeros primos
    Se genera script para obtener numeros primos e imprirlos en pantalla en revisión para generar un archivo que los guarde

---
+++ retonumerosprimos.py
@@ -0,0 +1,21 @@
+### Funcion para generar némeros primos del 1 al 250
+
+def es_primo(numeros):
+    if numeros < 2:
+        return False
+    for i in range(2, int(numeros**0.5) + 1):
+        if numeros % i == 0:
+            return False
+    return True
+    
+### Variables para tomar como inicio y fin     
+
+inicio = 1
+final = 250
+
+### Imprimir etiqueta de que rango se esta tomando para la conversion 
+print(f"Números primos entre {inicio} y {final}:\n")
+
+for numeros in range(inicio, final + 1):
+    if es_primo(numeros):
+        print(numeros)
